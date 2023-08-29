8e403642de317598ee4ff0b2f7aa4424dec16f8a
Rekxis <rekxis@gmx.de>
2023-08-29 10:49:11 +0000

    added data and select

---
+++ 04_SQL_DataImport/04c_db_SELECT.sql
@@ -0,0 +1,27 @@
+/* CRUD Create | Read | Update | Delete */  
+
+-- CREATE: siehe 04_ ...ALTER
+
+#SELECT * FROM boo.cats;
+
+/* UPDATE --> Änderung(en) in bestehender Tab. */
+#UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+#UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
+
+/* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
+#SELECT * FROM boo.cats; -- gesamte Tabelle
+#SELECT cat_name FROM boo.cats; -- einzelnes Feld
+#SELECT age,cat_name FROM boo.cats; -- Felder kombiniert
+#SELECT cat_name AS "Katzen" FROM boo.cats; -- Alias
+
+/*Filtern durch WHERE*/
+SELECT
+    cat_name AS "Katzen",
+    age AS "Alter"
+FROM boo.cats
+#WHERE id = 1
+#WHERE id > 1
+#WHERE fur_color = "grey"
+#WHERE fur_color = "aplinaweiss"
+WHERE age >= 30
+;
