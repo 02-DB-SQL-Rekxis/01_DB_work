df883c123117bce3ef456bc2e9db6c6b52dbd373
Rekxis <rekxis@gmx.de>
2023-08-30 08:56:56 +0000

    05/2 CONCAT/LIMIT

---
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -0,0 +1,27 @@
+/* QUERIES Basic */
+
+/*
+Felder abfragen
+Felder kombinieren
+Ausgabe beschränken
+*/
+
+-- Abfrage aller Spalten der Tab
+#SELECT * FROM stocks.ccc;
+
+-- Begrenzung: LIMIT
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombinationen aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT
+    ticker AS "SYM",
+    c_name AS Unternehmen,
+    #industry Branche
+    #sector Industriesektor
+    CONCAT(sector, "|", industry) AS "Operations",
+    CONCAT(yield, "|", dividend) AS "Dividenden"
+FROM stocks.ccc 
+LIMIT 10
+#LIMIT 1
+#LIMIT 200,10 -- offset
+;
